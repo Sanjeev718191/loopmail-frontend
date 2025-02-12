380f6be552206aef01b190f9f080430b680d0f04
Sanjeev Kumar <sanjeev718191@gmail.com>
2025-02-12 17:29:45 +0530

    Added Login, Signup, Home, Landing Pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './pages/login';
+import Signup from './pages/Signup';
+import Home from './pages/Home';
+import Landing from './pages/Landing';
 
 function App() {
   return (
     <div className="App">
-      <h1>Loopmail Home</h1>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/signup' element={<Home />} />
+        <Route path='/signup' element={<Landing />} />
+      </Routes>
     </div>
   );
 }
